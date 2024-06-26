e8d2a504ac00c448dd00d6d2465d773b9163919e
Dolev Dublon <127298010+ddublon@users.noreply.github.com>
2024-06-26 11:27:24 +0300

    lcjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+import { lightningChart } from "@arction/lcjs";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const lc = lightningChart({ license: "insert-your-license-here" });
+    const chart = lc.ChartXY({ container: "chart" });
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return () => {};
+  }, []);
+
+  return <div id="chart" style={{ width: "500px", height: "500px" }}></div>;
 }
 
-export default App
+export default App;
